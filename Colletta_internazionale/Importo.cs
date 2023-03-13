09edb2a74f3ee57c3d198ca4a6082ddb0733a467
Davihhhh <davidelin2005@gmail.com>
2023-03-13 08:26:37 +0100

    terminazine classi

--- Colletta_internazionale/Importo.cs
+++ Colletta_internazionale/Importo.cs
@@ -10,5 +10,46 @@ namespace Colletta_internazionale
     {
         private double _valore;
         private string _valuta;
+
+        public double Valore
+        {
+            get { return _valore; }
+            private set
+            {
+                if (value < 0)
+                    throw new Exception("Valore invalido");
+                else
+                    _valore = value;
+            }
+        }
+        public string Valuta
+        {
+            get { return _valuta; }
+            private set 
+            {
+                if (value == null || value.Length == 0)
+                    throw new Exception("Valuta invalida");
+                else _valuta = value;
+            }
+        }
+#pragma warning disable CS8618
+        public Importo(double importo, string valuta)
+#pragma warning restore CS8618
+        {
+            Valore = importo;
+            Valuta = valuta;
+        }
+
+        public bool Equals(Importo i)
+        {
+            if (i == null)
+                return false;
+            else if (this == i)
+                return true;
+            else if (Valore == i.Valore)
+                return true;
+            else 
+                return false;
+        }
     }
 }
--- Colletta_internazionale/Partecipante.cs
+++ Colletta_internazionale/Partecipante.cs
@@ -7,8 +7,45 @@ using System.Threading.Tasks;
 namespace Colletta_internazionale
 {
     internal class Partecipante
-    {
+    {      
         private string _key;
         private string _nome;
+
+        public string Key
+        {
+            get { return _key; }
+            private set {
+                if (value == null)
+                    throw new Exception("Chiave invalida");
+                else 
+                    _key = value; 
+            }
+        }
+        public string Nome
+        {
+            get { return _key; }
+            private set {
+                if (value == null || value.Length == 0)
+                    throw new Exception("Nome invalido");
+                else
+                    _key = value; 
+            }
+        }
+#pragma warning disable CS8618
+        public Partecipante(string nome)
+#pragma warning restore CS8618
+        {
+            Key = GetHashCode().ToString();
+            Nome = nome;         
+        }
+
+        public bool Equals(Partecipante p)
+        {
+            if (p == null)
+                return false;
+            else if (this == p)
+                return true;
+            else return false;
+        }
     }
 }
